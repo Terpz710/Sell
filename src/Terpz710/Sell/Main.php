66efa53db1030025e1fd2cc7818d766f8bc66620
BlueGamesNetwork <142659461+Terpz710@users.noreply.github.com>
2023-11-06 16:17:17 -0800

    BETA

--- src/Terpz710/Sell/Main.php
+++ src/Terpz710/Sell/Main.php
@@ -8,12 +8,15 @@ use pocketmine\plugin\PluginBase;
 use pocketmine\utils\Config;
 
 use Terpz710\Sell\Commands\SellCommand;
+use Terpz710\Sell\Economy\EconomyManager;
 
 class Main extends PluginBase {
 
     public function onEnable(): void {
         $this->saveResource("items.yml");
 
-        $this->getServer()->getCommandMap()->register("sell", new SellCommand($this));
+        $economyManager = new EconomyManager($this);
+
+        $this->getServer()->getCommandMap()->register("sell", new SellCommand($this, $economyManager));
     }
 }
