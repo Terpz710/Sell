30579330ff8e076030342cf5ed5ddee2917177f3
Jorge <134261497+Jorgebyte@users.noreply.github.com>
2024-08-29 22:21:17 -0600

    Fixed
    - An error in the economy was fixed
    - An unused feature was removed

--- src/Terpz710/Sell/Economy/EconomyManager.php
+++ src/Terpz710/Sell/Economy/EconomyManager.php
@@ -6,7 +6,6 @@ namespace Terpz710\Sell\Economy;
 
 use Closure;
 use pocketmine\player\Player;
-use pocketmine\plugin\Plugin;
 
 use onebone\economyapi\EconomyAPI;
 use cooldogedev\BedrockEconomy\api\type\ClosureAPI;
@@ -28,59 +27,47 @@ class EconomyManager {
         $this->plugin = $plugin;
         $manager = $this->plugin->getServer()->getPluginManager();
         $this->eco = $manager->getPlugin("EconomyAPI") ?? $manager->getPlugin("BedrockEconomy") ?? null;
-        $this->api = BedrockEconomyAPI::CLOSURE();
-        $this->currency = BedrockEconomy::getInstance()->getCurrency();
-        unset($manager);
+
+        if ($this->eco instanceof BedrockEconomy) {
+            $this->api = BedrockEconomyAPI::CLOSURE();
+            $this->currency = BedrockEconomy::getInstance()->getCurrency();
+        }
+
+        if ($this->eco === null) {
+            $this->plugin->getLogger()->warning("ERROR: No compatible economy plugin found. Please install EconomyAPI or BedrockEconomy");
+        }
     }
 
     public function getMoney(Player $player, Closure $callback): void {
+        if ($this->eco === null) {
+            $callback(0.0);
+            return;
+        }
+
         switch ($this->eco->getName()) {
             case "EconomyAPI":
                 $money = $this->eco->myMoney($player->getName());
-                assert(is_float($money));
-                $callback($money);
+                $callback(is_numeric($money) ? (float)$money : 0.0);
                 break;
             case "BedrockEconomy":
                 $entry = GlobalCache::ONLINE()->get($player->getName());
                 $callback($entry ? (float)"{$entry->amount}.{$entry->decimals}" : (float)"{$this->currency->defaultAmount}.{$this->currency->defaultDecimals}");
                 break;
             default:
-                $entry = GlobalCache::ONLINE()->get($player->getName());
-                $callback($entry ? (float)"{$entry->amount}.{$entry->decimals}" : (float)"{$this->currency->defaultAmount}.{$this->currency->defaultDecimals}");
-        }
-    }
-
-    public function reduceMoney(Player $player, int $amount, Closure $callback): void {
-        if ($this->eco == null) {
-            $this->plugin->getLogger()->warning("You don't have an Economy plugin");
-            return;
-        }
-        switch ($this->eco->getName()) {
-            case "EconomyAPI":
-                $callback($this->eco->reduceMoney($player->getName(), $amount) === EconomyAPI::RET_SUCCESS);
-                break;
-            case "BedrockEconomy":
-                $decimals = (int)(explode('.', strval($amount))[1] ?? 0);
-                $this->api->subtract(
-                    $player->getXuid(),
-                    $player->getName(),
-                    (int)$amount,
-                    $decimals,
-                    fn () => $callback(true),
-                    fn () => $callback(false)
-                );
-                break;
+                $callback(0.0);
         }
     }
 
     public function addMoney(Player $player, int $amount, Closure $callback): void {
-        if ($this->eco == null) {
+        if ($this->eco === null) {
             $this->plugin->getLogger()->warning("You don't have an Economy plugin");
+            $callback(false);
             return;
         }
+
         switch ($this->eco->getName()) {
             case "EconomyAPI":
-                $callback($this->eco->addMoney($player->getName(), $amount, EconomyAPI::RET_SUCCESS));
+                $callback($this->eco->addMoney($player->getName(), $amount) === EconomyAPI::RET_SUCCESS);
                 break;
             case "BedrockEconomy":
                 $decimals = (int)(explode('.', strval($amount))[1] ?? 0);
@@ -89,10 +76,12 @@ class EconomyManager {
                     $player->getName(),
                     (int)$amount,
                     $decimals,
-                    fn () => $callback(true),
-                    fn () => $callback(false)
+                    fn() => $callback(true),
+                    fn() => $callback(false)
                 );
                 break;
+            default:
+                $callback(false);
         }
     }
 }
