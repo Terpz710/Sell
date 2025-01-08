2397865b6efaa6411ac0e1029138a0c92e3b51bc
BlueGamesNetwork <142659461+Terpz710@users.noreply.github.com>
2025-01-07 20:07:19 -0800

    Add files via upload

---
+++ src/terpz710/sell/utils/Error.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace terpz710\sell\utils;
+
+final class Error {
+
+    const TYPE_CANNOT_SELL_ITEMS = "This message should never appear, if you're seeing this contact staff or the developer: github.com/Terpz710";
+    const TYPE_USE_COMMAND_INGAME = "This command can only be used in-game!";
+
+    //TODO: More error types
+
+}
---
+++ src/terpz710/sell/utils/Permission.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace terpz710\sell\utils;
+
+final class Permission {
+
+    const SELL_ITEM = "sell.items";
+    const SELL_HAND = "sell.hand";
+    const SELL_ALL = "sell.all";
+    const SELL_AUTO = "sell.autosell";
+
+}
